724b8424d0d4517af98afa744f88cf87300f8ef2
nhi1e <tnhile2004@gmail.com>
2024-01-20 14:13:47 -0800

    hsad

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,86 @@
+import Select from "react-select";
+import AP_list from "../data/AP_list";
+import makeAnimated from "react-select/animated";
+import { useState } from "react";
+
+const customSelect = {
+  control: (provided, state) => ({
+    ...provided,
+    background: "#fff",
+    borderColor: "#fff",
+    boxShadow: state.isFocused ? null : null,
+  }),
+
+  multiValue: (provided, state) => ({
+    ...provided,
+    backgroundColor: "#e5e7eb",
+    borderRadius: "13px",
+  }),
+  valueContainer: (provided, state) => ({
+    ...provided,
+    // height: "30px",
+    padding: "0 6px",
+    fontSize: "14px",
+  }),
+
+  input: (provided, state) => ({
+    ...provided,
+    margin: "0px",
+  }),
+  indicatorSeparator: (state) => ({
+    display: "none",
+  }),
+  indicatorsContainer: (provided, state) => ({
+    ...provided,
+    height: "30px",
+  }),
+  menuPortal: (base) => ({
+    ...base,
+    fontSize: "3px",
+  }),
+};
 export default function Navbar() {
-  // both buttons are just temp to see how it looks
+  const animatedComponents = makeAnimated();
+
+  const [collapse, setCollapse] = useState(false);
+
+  const handleCollapse = () => {
+    if (collapse === false) {
+      setCollapse(true);
+    } else {
+      setCollapse(false);
+    }
+  };
+
   return (
-    <>
-      <button>Prior credits</button> <buttons>Notes?</buttons>
-    </>
+    <div className="mt-6">
+      <div
+        onClick={handleCollapse}
+        className="mt-2 ml-5 border-b-2 flex hover:bg-gray transition-colors duration-200"
+      >
+        Pre-College
+        <img
+          src={collapse ? "public/arrow.jpg" : "public/arrow.jpg"}
+          className="ml-10 max-w-6"
+        />
+      </div>
+
+      <div>
+        {!collapse && (
+          <div className="px-6 py-4">
+            <Select
+              className="multi"
+              classNamePrefix="select"
+              placeholder="Pre-college credits"
+              options={AP_list}
+              styles={customSelect}
+              components={animatedComponents}
+              isMulti
+              closeMenuOnSelect={false}
+            />
+          </div>
+        )}
+      </div>
+    </div>
   );
 }
