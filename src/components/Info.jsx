b66ee362fb3b8bb9ac4a5baad012e5a142b07951
Waylon Williams <crazyocean9@gmail.com>
2024-01-20 22:00:07 -0800

    course info mapped to state var

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -4,17 +4,17 @@ export default function Info({ satisfied }) {
     <div className=" bg-black-dark1 mb-4 max-w-sm rounded overflow-hidden shadow-md">
       <div className="px-6 py-4">
         <div className="font-bold text-gray-dark text-ml mb-0.25">
-          Selected Course
+          {satisfied[19][0]}
         </div>
       </div>
       <div className="px-6  pb-2">
         <p className="text-gray-700 text-gray-dark text-sm pb-2">
-          Prerequisites: prereq info here
+          {satisfied[19][1]}
         </p>
         <div className="bg-gray-light justify-end py-2 rounded-2xl flex items-center">
           <div className="className=text-white bg-gray-dark hover:bg-gray font-medium rounded-xl text-xs px-2 py-1 focus:outline-none">
             <a
-              href="https://catalog.ucsc.edu/en/current/general-catalog/academic-units/baskin-engineering/computer-science-and-engineering/computer-science-bs/"
+              href={satisfied[19][2]}
               target="_blank"
               className="font-medium text-xs px-0 py-0.5 focus:outline-none mr-2 flex items-center" // Combined styles for link and flex container
             >
